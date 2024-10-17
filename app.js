62629ed15367d5013c3c5c98bf703c0ff1e11f3c
babyblue64 <abhay.raveendran2000@gmail.com>
2024-10-17 14:27:46 +0530

    changed paths to relative

--- app.js
+++ app.js
@@ -13,8 +13,12 @@ const testCasesRouter = require('./routes/testCasesRoute');
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/run', testCasesRouter);
-app.use('/questions', questionsRouter);
+app.use('/api/run', testCasesRouter);
+app.use('/api/questions', questionsRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
 
 server.listen( PORT, () => {
     console.log(`server at port ${PORT}`);
